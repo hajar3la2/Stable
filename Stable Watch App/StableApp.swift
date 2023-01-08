aad4ad5762df8ca42e7d9ea28f51d871cf973e0e
Hajar <hajar3la2@gmail.com>
2023-01-08 10:42:23 +0300

    Initial Commit

---
+++ Stable Watch App/StableApp.swift
@@ -0,0 +1,17 @@
+//
+//  StableApp.swift
+//  Stable Watch App
+//
+//  Created by Hajar on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Stable_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
